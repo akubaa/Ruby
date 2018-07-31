6e39ff2d7735469255fa2c54e8ef0f56b52a49c0
tommy.potocki@melcogot.com <tommy.potocki@melcogot.com>
2018-07-31 09:45:05 +0200

    Added verbose flag and small bat file to start two loggers with different COM ports

--- com.rb
+++ com.rb
@@ -1,18 +1,24 @@
 #!/usr/bin/ruby
 require 'serialport'
 
-  time = Time.now
-  t = "#{time.year}_#{time.month}_#{time.day}_#{time.hour}_#{time.min}.log"
-  f = File.open(t, 'a+')
-
-  i = ARGV[0]
-  ser = SerialPort.new i, 460800
-  while true
-    while (buff = ser.gets.chomp)
-      f.puts buff
+time = Time.now
+t = "#{time.year}_#{time.month}_#{time.day}_#{time.hour}_#{time.min}.log"
+f = File.open(t, 'a+')
+verbose = ARGV[1]
+if verbose == nil
+  puts "Start logging to #{t}"
+  puts "Add -v flag for output of serial data in terminal"
+end
+i = ARGV[0]
+ser = SerialPort.new i, 460800
+while true
+  while (buff = ser.gets.chomp)
+    f.puts buff
+    if ARGV[1] == '-v'
       puts buff
     end
   end
+end
 
 
 
---
+++ multi_com.bat
@@ -0,0 +1,4 @@
+ruby com.rb %1
+ruby com.rb %2
+
+echo 'Started two serial loggers'
