f45209bb08b5d470950c64e6d1f1a356993fa391
tommy.potocki@melcogot.com <tommy.potocki@melcogot.com>
2018-07-31 11:05:40 +0200

    Added some exception handling

--- com.rb
+++ com.rb
@@ -1,24 +1,63 @@
 #!/usr/bin/ruby
 require 'serialport'
 
-time = Time.now
-t = "#{time.year}_#{time.month}_#{time.day}_#{time.hour}_#{time.min}.log"
-f = File.open(t, 'a+')
-verbose = ARGV[1]
-if verbose == nil
-  puts "Start logging to #{t}"
-  puts "Add -v flag for output of serial data in terminal"
-end
 i = ARGV[0]
-ser = SerialPort.new i, 460800
-while true
-  while (buff = ser.gets.chomp)
-    f.puts buff
-    if ARGV[1] == '-v'
-      puts buff
+verbose = ARGV[1]
+
+def setup (arg)
+#460800
+  begin
+    @ser = SerialPort.new arg, 115200
+  rescue
+    puts "Not possible to open #{i}"
+  end
+end
+
+def logfile
+  begin
+    time = Time.now
+    t = "#{time.year}_#{time.month}_#{time.day}_#{time.hour}_#{time.min}.log"
+    @file = File.open(t, 'a+')
+  rescue
+    puts "Not possible to create file"
+    puts "Please retry to run com.rb"
+  end
+end
+
+def log
+  while true
+    while (buff = @ser.gets.chomp)
+      @file.puts buff
+      if ARGV[1] == '-v'
+        puts buff
+      end
     end
   end
 end
+
+def check (var)
+  begin
+    if var != nil and var != '-v'
+      raise 'Invalid flag'
+    end
+    if var == nil
+      puts "Start logging"
+      puts "Add -v flag for output of serial data in terminal"
+    end
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace.inspect
+  end
+end
+
+check verbose
+setup i
+logfile
+log
+
+
+
+
 
 
 
