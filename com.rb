bcce992939823cd4c59656a27f5122d5724515bc
tommy.potocki@melcogot.com <tommy.potocki@melcogot.com>
2018-07-31 11:10:08 +0200

    Rubocop a few changes

--- com.rb
+++ com.rb
@@ -4,61 +4,44 @@ require 'serialport'
 i = ARGV[0]
 verbose = ARGV[1]
 
-def setup (arg)
-#460800
-  begin
-    @ser = SerialPort.new arg, 115200
-  rescue
-    puts "Not possible to open #{i}"
-  end
+def setup(arg)
+  # 460800
+
+  @ser = SerialPort.new arg, 115_200
+rescue StandardError
+  puts "Not possible to open #{i}"
 end
 
 def logfile
-  begin
-    time = Time.now
-    t = "#{time.year}_#{time.month}_#{time.day}_#{time.hour}_#{time.min}.log"
-    @file = File.open(t, 'a+')
-  rescue
-    puts "Not possible to create file"
-    puts "Please retry to run com.rb"
-  end
+  time = Time.now
+  t = "#{time.year}_#{time.month}_#{time.day}_#{time.hour}_#{time.min}.log"
+  @file = File.open(t, 'a+')
+rescue StandardError
+  puts 'Not possible to create file'
+  puts 'Please retry to run com.rb'
 end
 
 def log
-  while true
+  loop do
     while (buff = @ser.gets.chomp)
       @file.puts buff
-      if ARGV[1] == '-v'
-        puts buff
-      end
+      puts buff if ARGV[1] == '-v'
     end
   end
 end
 
-def check (var)
-  begin
-    if var != nil and var != '-v'
-      raise 'Invalid flag'
-    end
-    if var == nil
-      puts "Start logging"
-      puts "Add -v flag for output of serial data in terminal"
-    end
-  rescue Exception => e
-    puts e.message
-    puts e.backtrace.inspect
+def check(var)
+  raise 'Invalid flag' if !var.nil? && (var != '-v')
+  if var.nil?
+    puts 'Start logging'
+    puts 'Add -v flag for output of serial data in terminal'
   end
+rescue StandardError => e
+  puts e.message
+  puts e.backtrace.inspect
 end
 
 check verbose
 setup i
 logfile
 log
-
-
-
-
-
-
-
-
